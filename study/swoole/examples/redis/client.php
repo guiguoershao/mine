3f5c71be01fea17b33ebaab6643d6cd6f1ea655c
fengyan <guiguoershao@163.com>
2019-05-08 18:02:32 +0800

    swoole学习之旅-swoole-redis

---
+++ study/swoole/examples/redis/client.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../bootstrap.php';
+
+$client = new swoole_redis();
+
+$client->connect('127.0.0.1', 6379, function (swoole_redis $swoole_redis, $result) {
+    if ($result === false) {
+        dump("连接redis server服务失败");
+        return;
+    }
+    dump("connect redis server success");
+    $swoole_redis->set('test', 'test', function (swoole_redis $swoole_redis, $result) {
+        dump($result);
+
+    });
+
+    $swoole_redis->set('test:1', 'test-1', function (swoole_redis $swoole_redis, $result) {
+        dump($result);
+    });
+
+});
