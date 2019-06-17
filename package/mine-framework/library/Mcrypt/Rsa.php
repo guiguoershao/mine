3723d7c460f71d4be0d637caa81fc33844bdd03c
fengyan <guiguoershao@163.com>
2019-06-17 11:33:58 +0800

    Rsa非对称加密

--- package/mine-framework/library/Mcrypt/Rsa.php
+++ package/mine-framework/library/Mcrypt/Rsa.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * 生成私钥和 公钥
+ * openssl genrsa -out rsa_private_key.pem 1024
+ * openssl pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -nocrypt -out private_key.pem
+ * openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
+ */
 
 namespace Library\Mcrypt;
 
