4350c519c6bee71a405a2f8ede79e509a80ac7f1
fengyan <guiguoershao@163.com>
2019-06-17 14:37:17 +0800

    优化框架

--- package/mine-framework/app/Bootstrap.php
+++ package/mine-framework/app/Bootstrap.php
@@ -19,18 +19,18 @@ class Bootstrap implements IBootstrap
 
     /**
      * 启动初始化
-     * @param Container $app
+     * @param Container $container
      * @return Container
      */
-    public function boot(Container $app)
+    public function boot(Container $container)
     {
-        $app->register(Http::class, function () {
+        $container->register(Http::class, function () {
             return new Http();
         })->resolverProviders([
             'app' => \App\Providers\AppProvider::class,
             'route' => \App\Providers\RouteProvider::class,
         ]);
 
-        return $app;
+        return $container;
     }
 }
