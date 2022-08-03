2f12c22345c26c1487b5f542bd3e5c7b392dc5b0
fengyan <guiguoershao@163.com>
2022-08-03 15:10:11 +0800

    feat golang 基础学习

--- study/golang/test-mongodb.go
+++ study/golang/test-mongodb.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func initDB() {
+	clientOptions := options.Client().ApplyURI("mongodb://120.77.156.30:23307")
+
+	var err error
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+}
+
+func main() {
+	initDB()
 }
