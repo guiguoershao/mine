ac92f2915398c2407f3f7289c8cf31d3bcd1af33
fengyan <guiguoershao@163.com>
2022-08-09 17:36:47 +0800

    feat 测试

--- study/golang-topic/golang-redis.go
+++ study/golang-topic/golang-redis.go
@@ -8,21 +8,25 @@ import (
 
 var ctx = context.Background()
 
-//noinspection ALL
 func ExampleClient() {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "120.77.156.30:26379",
 		Password: "rvEBhbJP9m@YmaKL",
 		DB:       0,
 	})
+	fmt.Printf("连接redis成功: %v\n", rdb)
 	err := rdb.Set(ctx, "name", "golang-teck-stack.com", 0).Err()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
+		//panic(err)
 	}
 
 	val, err := rdb.Get(ctx, "name").Result()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
+		//panic(err)
 	}
 	fmt.Println("name", val)
 
@@ -30,10 +34,13 @@ func ExampleClient() {
 	if err == redis.Nil {
 		fmt.Println("key2 does not exist")
 	} else if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
+		//panic(err)
 	} else {
 		fmt.Println("key2", val2)
 	}
+	defer rdb.Close()
 }
 
 func main() {
