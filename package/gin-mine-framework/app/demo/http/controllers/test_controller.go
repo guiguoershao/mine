0f8857e5d9b273987b046733b5791f8cceaa591a
fengyan <guiguoershao@163.com>
2022-08-12 10:05:25 +0800

    feat gin-framework 响应输出

--- package/gin-mine-framework/app/demo/http/controllers/test_controller.go
+++ package/gin-mine-framework/app/demo/http/controllers/test_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"gin-mine/framework"
 	"gin-mine/framework/controller"
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +11,6 @@ type TestController struct {
 }
 
 func (ctrl TestController) T1(ctx *gin.Context) {
-	//framework.Core().Redis().Ping()
+	framework.Logger().Info("测试日志")
 	ctrl.Jump.Ping(ctx)
 }
