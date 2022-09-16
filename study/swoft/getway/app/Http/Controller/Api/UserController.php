55fe9870c0d9d3fbb7cb8461275df1eafdaf413e
fengyan <guiguoershao@163.com>
2022-09-16 10:34:28 +0800

    feat hyperf 学习

---
+++ study/swoft/getway/app/Http/Controller/Api/UserController.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+/**
+ * 用户控制器
+ *
+ * @link https://swoft.org
+ * @document https://swoft.org/docs
+ * @contact group@swoft.org
+ * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace App\Http\Controller;
+
+use Swoft\Http\Server\Annotation\Mapping\Controller;
+use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
+use Swoft\Http\Server\Annotation\Mapping\RequestMethod;
+// use Swoft\Http\Message\Response;
+
+/**
+ * Class Api/UserController
+ *
+ * @Controller(prefix="/api/User")
+ * @package App\Http\Controller
+ */
+class UserController{
+    /**
+     * Get data list. access uri path: /api/User
+     * @RequestMapping(route="/api/User", method=RequestMethod::GET)
+     * @return array
+     */
+    public function index(): array
+    {
+        return ['item0', 'item1'];
+    }
+
+    /**
+     * Get one by ID. access uri path: /api/User/{id}
+     * @RequestMapping(route="{id}", method=RequestMethod::GET)
+     * @return array
+     */
+    public function get(): array
+    {
+        return ['item0'];
+    }
+
+    /**
+     * Create a new record. access uri path: /api/User
+     * @RequestMapping(route="/api/User", method=RequestMethod::POST)
+     * @return array
+     */
+    public function post(): array
+    {
+        return ['id' => 2];
+    }
+
+    /**
+     * Update one by ID. access uri path: /api/User/{id}
+     * @RequestMapping(route="{id}", method=RequestMethod::PUT)
+     * @return array
+     */
+    public function put(): array
+    {
+        return ['id' => 1];
+    }
+
+    /**
+     * Delete one by ID. access uri path: /api/User/{id}
+     * @RequestMapping(route="{id}", method=RequestMethod::DELETE)
+     * @return array
+     */
+    public function del(): array
+    {
+        return ['id' => 1];
+    }
+}
