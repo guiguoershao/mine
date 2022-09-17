f7b13b3335371d0b2cc651d7e3402f4dc4233cd0
FengYan <guiguoershao@163.com>
2022-09-17 15:07:24 +0800

    feat 学习 hyperf

--- study/hyperf/my-hyperf-app/app/Aspect/FooAspect.php
+++ study/hyperf/my-hyperf-app/app/Aspect/FooAspect.php
@@ -31,9 +31,7 @@ class FooAspect extends AbstractAspect
         // 切面切入后，执行对应的方法会由此来负责
         // $proceedingJoinPoint 为连接点，通过该类的 process() 方法调用原方法并获得结果
         // 在调用前进行某些处理
-        var_dump(1);
         $result = $proceedingJoinPoint->process();
-        var_dump(2);
         // 在调用后进行某些处理
         return $result;
     }
